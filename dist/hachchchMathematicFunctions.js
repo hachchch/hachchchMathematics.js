e987ebf137cd20b27be0058b88eb3b5b477c5452
nsukasuhamidori <137990341+hachchch@users.noreply.github.com>
2024-08-29 15:03:08 +0900

    Create hachchchMathematicFunctions.js

---
+++ dist/hachchchMathematicFunctions.js
@@ -0,0 +1,36 @@
+class mathematics{
+  sum(K,N,gen){
+  let ans=0;
+  for(k=K;k<=N;++k){
+    ans+=eval(gen);
+  }
+  return ans;
+}
+prod(K,N,gen){
+  let ans=1;
+  for(k=K;k<=N;++k){
+    ans=ans*eval(gen);
+  }
+  return ans;
+}
+fact(N){
+  return prod(1,N,"k");
+}
+bC(n,r){
+  return fact(n)/(fact(r)*fact(n-r))
+}
+mean(arr){
+  let ans=0;
+  for(k=0;k<arr.length;++k){
+    ans+=eval("arr[k]");
+  }
+  return ans/arr.length;
+}
+geomean(arr){
+  let ans=1;
+  for(k=0;k<arr.length;++k){
+    ans=ans*eval("arr[k]");
+  }
+  return Math.pow(ans,1/arr.length);
+}
+}
